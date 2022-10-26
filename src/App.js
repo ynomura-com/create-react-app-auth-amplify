c377d3099d85b00cb4150965b8ad65baccbec559
NOMURA Yukinori <ynomura@mac.com>
2022-10-26 10:43:29 +0900

    Back to Original

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import '@aws-amplify/ui-react/styles.css'; //☆
-import { Amplify } from 'aws-amplify'; //☆
-import { withAuthenticator } from '@aws-amplify/ui-react'; // ☆
-import aws_exports from './aws-exports'; //☆
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
-Amplify.configure(aws_exports); //☆
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sample React app</h1>
-        <h2><a className="App-link" href="."
-          onClick={Auth.signOut}>
-          Sign Out
-        </a></h2>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AmplifySignOut />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
 
 export default withAuthenticator(App);
